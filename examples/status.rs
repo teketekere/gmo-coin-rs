ee73d6c988af53bfa74ace2b4c3341809c1c02c3
teketekere <hujikoseina@gmail.com>
2020-11-08 14:11:20 +0000

    ✨ かかった時間を測定するようにした

--- examples/status.rs
+++ examples/status.rs
@@ -2,6 +2,8 @@ use gmo_coin_rs::error::Error;
 use gmo_coin_rs::http_client::Reqwest;
 use gmo_coin_rs::public::status::*;
 
+use std::time::Instant;
+
 /// 取引所ステータスを取得するAPIのExample
 ///
 /// # Example
@@ -12,8 +14,15 @@ use gmo_coin_rs::public::status::*;
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let http_client = Reqwest;
+    let start = Instant::now();
     let response = get_status(http_client).await?;
+    let end = start.elapsed();
 
+    println!(
+        "かかった時間: {}.{:05} [seconds]",
+        end.as_secs(),
+        end.subsec_micros() / 1000000
+    );
     println!("取引所ステータス: {}\n", response.status());
     println!(
         "HTTPステータスコード: {}\nステータスコード: {}\nAPIを呼び出した時間: {}",
