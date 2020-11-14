a30a7179617c93f639668a933b9187403f0f2717
teketekere <hujikoseina@gmail.com>
2020-11-14 17:55:46 +0900

    :sparkles: 資産残高APIを実装した

--- src/private.rs
+++ src/private.rs
@@ -1,9 +1,11 @@
 //! Private APIを実装する。
 
+pub mod assets;
 pub mod margin;
 
 use crate::error::Error;
 use crate::http_client::HttpClient;
+use crate::private::assets::{get_assets, Assets};
 use crate::private::margin::{get_margin, Margin};
 use crate::response::RestResponse;
 
@@ -27,6 +29,23 @@ impl<T: HttpClient + std::marker::Sync + std::marker::Send> PrivateAPI<T> {
         secret_key: &str,
     ) -> Result<RestResponse<Margin>, Error> {
         let response = get_margin(&self.http_client, &api_key, &secret_key).await?;
+        Ok(response)
+    }
+
+    /// 資産残高APIを呼び出す。
+    ///
+    /// # Arguments
+    ///
+    /// * `http_client` - Http client
+    /// * `api_key` - GMOコインのAPIキー。
+    /// * `secret_key` - GMOコインのAPIシークレット。
+    ///
+    pub async fn assets(
+        &self,
+        api_key: &str,
+        secret_key: &str,
+    ) -> Result<RestResponse<Assets>, Error> {
+        let response = get_assets(&self.http_client, &api_key, &secret_key).await?;
         Ok(response)
     }
 }
---
+++ src/private/assets.rs
@@ -0,0 +1,129 @@
+//! 資産残高APIを実装する。
+
+use crate::end_point::*;
+use crate::error::Error;
+use crate::headers::Headers;
+use crate::http_client::*;
+use crate::json::*;
+use crate::response::*;
+use crate::secret::Secret;
+use crate::timestamp::get_timestamp;
+use chrono::{DateTime, Utc};
+use serde::Deserialize;
+
+/// 資産残高APIのパス。
+const ASSETS_API_PATH: &str = "/v1/account/assets";
+
+/// 資産残高APIを呼び出すときのメソッド。
+const ASSETS_API_METHOD: &str = "GET";
+
+/// 資産残高APIから返ってくるレスポンスのうち`data`の部分を格納する構造体。
+#[derive(Deserialize)]
+#[allow(non_snake_case)]
+pub struct Data {
+    /// 残高。
+    #[serde(deserialize_with = "str_to_f64")]
+    pub amount: f64,
+
+    /// 利用可能金額。残高 - 出金予定額。
+    #[serde(deserialize_with = "str_to_f64")]
+    pub available: f64,
+
+    /// 円転レート。
+    #[serde(deserialize_with = "str_to_f64")]
+    pub conversionRate: f64,
+
+    /// 銘柄名。
+    pub symbol: String,
+}
+
+impl Data {
+    /// 残高を円で取得する。
+    pub fn amount_as_jpy(&self) -> f64 {
+        self.amount * self.conversionRate
+    }
+
+    /// 利用可能金額を円で取得する。
+    pub fn available_as_jpy(&self) -> f64 {
+        self.available * self.conversionRate
+    }
+}
+
+/// 資産残高APIから返ってくるレスポンスを格納する構造体。
+#[derive(Deserialize)]
+pub struct Assets {
+    pub status: i16,
+    #[serde(deserialize_with = "gmo_timestamp_to_chrono_timestamp")]
+    pub responsetime: DateTime<Utc>,
+    pub data: Vec<Data>,
+}
+
+impl RestResponse<Assets> {
+    /// 資産残高が格納された配列を取得する。
+    pub fn assets(&self) -> &Vec<Data> {
+        &self.body.data
+    }
+}
+
+/// 資産残高APIを呼び出す。
+pub async fn get_assets(
+    http_client: &impl HttpClient,
+    api_key: &str,
+    secret_key: &str,
+) -> Result<RestResponse<Assets>, Error> {
+    let url = format!("{}{}", PRIVATE_ENDPOINT, ASSETS_API_PATH,);
+    let timestamp = get_timestamp();
+    let text = format!("{}{}{}", timestamp, ASSETS_API_METHOD, ASSETS_API_PATH);
+    let secret = Secret::create(&api_key, &secret_key, &text);
+    let headers = Headers::create_get_headers(&secret, timestamp);
+    let response = http_client.get(url, &headers).await?;
+    parse_from_http_response::<Assets>(&response)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::http_client::tests::InmemClient;
+    use chrono::SecondsFormat;
+
+    const SAMPLE_RESPONSE: &str = r#"
+    {
+        "status": 0,
+        "data": [
+          {
+            "amount": "993982448",
+            "available": "993982448",
+            "conversionRate": "1",
+            "symbol": "JPY"
+          },
+          {
+            "amount": "4.0002",
+            "available": "4.0002",
+            "conversionRate": "859614",
+            "symbol": "BTC"
+          }
+        ],
+        "responsetime": "2019-03-19T02:15:06.055Z"
+    }
+          "#;
+
+    #[tokio::test]
+    async fn should_return_ok_when_http_client_returns_correct_response() {
+        let body = SAMPLE_RESPONSE;
+        let http_client = InmemClient {
+            http_status_code: 200,
+            body_text: body.to_string(),
+            return_error: false,
+        };
+        let resp = get_assets(&http_client, "apikey", "seckey").await.unwrap();
+        assert_eq!(resp.http_status_code, 200);
+        assert_eq!(resp.body.status, 0);
+        assert_eq!(
+            resp.body
+                .responsetime
+                .to_rfc3339_opts(SecondsFormat::Millis, true),
+            "2019-03-19T02:15:06.055Z"
+        );
+        assert_eq!(resp.assets().len(), 2);
+    }
+}
