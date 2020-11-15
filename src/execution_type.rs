f8b0c4e21d267b9b728dcbfca6eb0b1c574e522d
teketekere <hujikoseina@gmail.com>
2020-11-15 19:21:40 +0900

    :sparkles: 注文方法をenumで定義した

--- src/execution_type.rs
+++ src/execution_type.rs
@@ -1,6 +1,18 @@
 //! 注文方法を定義する。
 
-/// 成り行き注文
+/// 注文方法
+pub enum ExecutionType {
+    /// 成行注文
+    Market,
+
+    /// 指値注文
+    Limit,
+
+    /// 逆指値注文
+    Stop,
+}
+
+/// 成行注文
 pub const MARKET_ORDER: &str = "MARKET";
 
 /// 指値注文
@@ -8,3 +20,14 @@ pub const LIMIT_ORDER: &str = "LIMIT";
 
 /// 逆指値注文
 pub const STOP_ORDER: &str = "STOP";
+
+impl ExecutionType {
+    /// 注文方法を文字列に変換する。
+    pub fn to_string(&self) -> &str {
+        match self {
+            ExecutionType::Market => MARKET_ORDER,
+            ExecutionType::Limit => LIMIT_ORDER,
+            ExecutionType::Stop => STOP_ORDER,
+        }
+    }
+}
