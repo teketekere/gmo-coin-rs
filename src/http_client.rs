da2755f6e723272409cc5c4641d625c9f2aa7610
teketekere <hujikoseina@gmail.com>
2020-11-15 20:36:07 +0900

    :sparkles: POSTに対応

--- src/http_client.rs
+++ src/http_client.rs
@@ -4,11 +4,18 @@ use crate::error::*;
 use crate::headers::Headers;
 use crate::response::*;
 use async_trait::async_trait;
+use serde_json::Value;
 
 /// HTTPクライアントのtrait。GET, POSTとか。
 #[async_trait]
 pub trait HttpClient {
     async fn get(&self, url: String, headers: &Headers) -> Result<RawResponse, Error>;
+    async fn post(
+        &self,
+        url: String,
+        headers: &Headers,
+        parameters: &Value,
+    ) -> Result<RawResponse, Error>;
 }
 
 /// ネットワークアクセス時に用いるHttpクライアント。
@@ -29,6 +36,25 @@ impl HttpClient for Reqwest {
             body_text: (response.text().await?),
         })
     }
+
+    async fn post(
+        &self,
+        url: String,
+        headers: &Headers,
+        parameters: &Value,
+    ) -> Result<RawResponse, Error> {
+        let mut request_builder = reqwest::Client::new().post(reqwest::Url::parse(&url)?);
+        for (key, value) in headers {
+            request_builder = request_builder.header(key, value);
+        }
+
+        let request_builder = request_builder.json(&parameters);
+        let response = request_builder.send().await?;
+        Ok(RawResponse {
+            http_status_code: (response.status().as_u16()),
+            body_text: (response.text().await?),
+        })
+    }
 }
 
 #[cfg(test)]
@@ -42,10 +68,9 @@ pub mod tests {
         pub return_error: bool,
     }
 
-    #[async_trait]
-    impl HttpClient for InmemClient {
-        async fn get(&self, _url: String, _headers: &Headers) -> Result<RawResponse, Error> {
-            if (self.return_error) {
+    impl InmemClient {
+        async fn return_result(&self) -> Result<RawResponse, Error> {
+            if self.return_error {
                 return Err(Error::UnknownError {});
             }
 
@@ -53,6 +78,22 @@ pub mod tests {
                 http_status_code: (self.http_status_code),
                 body_text: (self.body_text.clone()),
             })
+        }
+    }
+
+    #[async_trait]
+    impl HttpClient for InmemClient {
+        async fn get(&self, _url: String, _headers: &Headers) -> Result<RawResponse, Error> {
+            self.return_result().await
+        }
+
+        async fn post(
+            &self,
+            _url: String,
+            _headers: &Headers,
+            _parameters: &Value,
+        ) -> Result<RawResponse, Error> {
+            self.return_result().await
         }
     }
 }
